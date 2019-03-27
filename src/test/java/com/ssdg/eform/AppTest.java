bc89617f0230b24b6c821547db3c1105980ac5dc
kamal <kamal47thakur@gmail.com>
2019-03-27 19:08:30 +0530

    add Java Priject1

---
+++ src/test/java/com/ssdg/eform/AppTest.java
@@ -0,0 +1,20 @@
+package com.ssdg.eform;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
